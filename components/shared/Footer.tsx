9810237847a3afdaf27eb0de8c4e3018e657256d
Antony Carrasco Cunya <coltmoonsac@gmail.com>
2024-01-17 23:58:17 -0500

    Add footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default Footer
+const Footer = () => {
+	return (
+		<footer className="border-t">
+			<div className='wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row'>
+				<Link href="/">
+					<Image
+						src="/assets/images/logo.svg"
+						alt="logo"
+						width={128}
+						height={38}
+					/>
+				</Link>
+
+        <p>2023 Evently. All Rights reserved.</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
